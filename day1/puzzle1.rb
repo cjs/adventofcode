4980e367d186cb998b031c3bd5234efaa82c4a8d
Craig Steinberger <cjs@github.com>
2016-12-01 19:11:30 -0500

    refactor, and solve part 2

--- day1/puzzle1.rb
+++ day1/puzzle1.rb
@@ -3,49 +3,97 @@
 # Read and store input file into an array
 #
 
+# Reperesent a position
 class Position
   attr_accessor :xloc, :yloc, :direction
 
-  def initialize
-    self.xloc = 0
-    self.yloc = 0
-    self.direction = 0
+  def initialize(x, y, dir)
+    self.xloc = x
+    self.yloc = y
+    self.direction = dir
+    @position_history = ['X:0 Y:0']
   end
 
   def to_s
-    directions = ['N', 'E', 'S', 'W']
-    'X: ' + @xloc.to_s + ' Y: ' + self.yloc.to_s + ' Facing: ' + directions[self.direction / 90]
+    directions = %w(N E S W)
+    'X: ' + @xloc.to_s + ' Y: ' + @yloc.to_s + ' Facing: ' + directions[@direction / 90]
   end
 
   def distance
     @xloc.abs + @yloc.abs
   end
 
-  def navigate(instruction)
+  def blocks_from(start_pos)
+    (@xloc - start_pos.xloc).abs + (@yloc - start_pos.yloc).abs
+  end
+
+  def position
+    'X:' + @xloc.to_s + ' Y:' + @yloc.to_s
+  end
+end
+
+# Perform Navigation, keeping track of each position touched
+class Navigation
+  attr_accessor :position_history, :start_position, :current_position
+  def initialize(pos)
+    @start_position = pos
+    @current_position = pos
+    @position_history = [pos]
+  end
+
+  def move(instruction)
     turn = instruction.slice!(0)
     case turn
-    when "R"
-      @direction = ((@direction + 90) % 360).abs
-    when "L"
-      @direction = ((@direction - 90) % 360).abs
+    when 'R'
+      facing = ((@current_position.direction + 90) % 360).abs
+    when 'L'
+      facing = ((@current_position.direction - 90) % 360).abs
     else
       raise 'Bad Direction'
     end
-    @xloc = @xloc + Math::sin(@direction * Math::PI / 180).round * instruction.to_i
-    @yloc = @yloc + Math::cos(@direction * Math::PI / 180).round * instruction.to_i
+    xlocnew = @current_position.xloc + Math::sin(facing * Math::PI / 180).round * instruction.to_i
+    ylocnew = @current_position.yloc + Math::cos(facing * Math::PI / 180).round * instruction.to_i
+    if xlocnew != @current_position.xloc
+      steps = (@current_position.xloc..xlocnew).to_a.drop(1)
+      steps.each do |x|
+        @position_history << Position.new(x, @current_position.yloc, facing)
+      end
+    else
+      steps = (@current_position.yloc..ylocnew).to_a.drop(1)
+      steps.each do |y|
+        @position_history << Position.new(@current_position.xloc, y, facing)
+      end
+    end
+    @current_position = Position.new(xlocnew, ylocnew, facing)
   end
-
 end
 
-
-p = Position.new
+nav = Navigation.new(Position.new(0, 0, 0))
 instructions = File.read('input.txt').chomp.split(', ')
 
+#instructions = %w(R8 R4 R4 R8)
+p nav.current_position.to_s
 
 instructions.each do |i|
-  p.navigate(i)
+  nav.move(i)
 end
 
-puts p.to_s
-puts p.distance.to_s + ' blocks away'
+print "Ended up #{nav.current_position.blocks_from(nav.start_position)} blocks away\n"
+double_visit = nil
+position_counts = {}
+nav.position_history.each do |pos|
+  unless double_visit
+    if position_counts.key? pos.position
+      puts 'double visit found'
+      double_visit = pos
+    else
+      position_counts[pos.position] = 1
 
+    end
+  end
+end
+
+if double_visit
+  print "#{double_visit.position} was first visited twice."
+  print "It is #{double_visit.blocks_from(nav.start_position)} blocks away.\n"
+end
