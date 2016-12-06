4f84325018fd2c2a6be9fe5a2493e363e1de81c4
Craig Steinberger <cjs@github.com>
2016-12-06 14:01:31 -0500

    part 1 solution

---
+++ 2016/day6/puzzle6.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#
+
+freqs = Array.new(8) { ('a'..'z').to_a.product([0]).to_h }
+
+msgs = File.open('input')
+
+msgs.each do |msg|
+  msg.chomp.split('').each_index do |i|
+    freqs[i][msg[i]] += 1
+  end
+end
+
+
+ecmsg = ''
+freqs.each do |freq|
+  ecmsg.concat(freq.to_a.sort_by {|char, count| -count}[0][0])
+end
+
+p "Part 1: #{ecmsg}"
