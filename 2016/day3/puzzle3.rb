c7e5860487d8ca665e2bfe202e6245de5656c774
Craig Steinberger <cjs@github.com>
2016-12-06 05:43:39 -0500

    part 2 solution

--- 2016/day3/puzzle3.rb
+++ 2016/day3/puzzle3.rb
@@ -10,4 +10,30 @@ instructions.each do |line|
   a, b, c = line.chomp.split(' ').map(&:to_i)
   counter = counter + 1 if validtriangle(a, b, c)
 end
+
+p 'Part 1'
 p counter
+
+instructions.rewind
+counter = 0
+index = 0
+t = Array.new(3)
+t.each_index do |i|
+  t[i] = Array.new(3)
+end
+instructions.each do |line|
+  t[0][index], t[1][index], t[2][index] = line.chomp.split(' ').map(&:to_i)
+  if index == 2
+    t.each_index do |i|
+      a, b, c = t[i]
+      counter = counter + 1 if validtriangle(a,b,c)
+    end
+    index = 0
+  else
+    index = index + 1
+  end
+end
+
+p 'Part 2'
+p counter
+
