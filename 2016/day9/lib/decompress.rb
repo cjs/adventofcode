1074e0684d6f547836f1a4c858e154ebec19f82b
Craig Steinberger <cjs@github.com>
2016-12-12 13:53:51 -0500

    clean up part 1 debugging

--- 2016/day9/lib/decompress.rb
+++ 2016/day9/lib/decompress.rb
@@ -1,23 +1,18 @@
 class String
   def decompress
     s = self.dup
-    debug = false
-    p "debugging #{s}" if debug
     re =  /\((\d+)x(\d+)\)/
     ds = ''
     until  s.empty? do
       output, marker, rem = s.partition(re)
-      p "output: #{output} marker: #{marker} rem: #{rem}"if debug
       ds << output unless output.empty?
       unless marker.empty?
         chars, repeat = marker.match(re).captures
         chunk = rem[0, Integer(chars)]
-        p "chunk: #{chunk}" if debug
         Integer(repeat).times { ds << chunk }
         rem = rem[Integer(chars)..-1]
       end
       s.replace(rem)
-      p "End Loop: #{s.inspect}" if debug
     end
     ds
   end
