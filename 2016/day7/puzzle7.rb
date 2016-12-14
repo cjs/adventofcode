c924b7cbc2d5395c0e38cc0865a3827498c76950
Craig Steinberger <cjs@github.com>
2016-12-14 16:57:46 -0500

    clean up rubocop warnings

--- 2016/day7/puzzle7.rb
+++ 2016/day7/puzzle7.rb
@@ -4,11 +4,13 @@ require './lib/ip7'
 
 tls_count = File.open('input').reduce(0) do |count, ip| 
   IPaddress.new(ip.chomp).support_tls? ? count += 1 : count
+  count
 end
 
 p "Part 1: #{tls_count}"
 
 tls_count = File.open('input').reduce(0) do |count, ip| 
     IPaddress.new(ip.chomp).support_ssl? ? count += 1 : count
+    count
 end
 p "Part 2: #{tls_count}"
