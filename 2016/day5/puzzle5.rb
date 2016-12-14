caa0677f338a79a785eb6f83629c5722ea8e2905
Craig Steinberger <cjs@github.com>
2016-12-14 16:53:36 -0500

    remove unused index

--- 2016/day5/puzzle5.rb
+++ 2016/day5/puzzle5.rb
@@ -15,7 +15,7 @@ door_id = 'reyedfim'
 
 start_index = 0
 password = ''
-0.upto(7) do |i|
+0.upto(7) do 
   digest, found_index = find_hash(door_id, start_index)
   password.concat(digest[5].to_s)
   start_index = found_index.next
